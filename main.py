7f5034f88a6934bbb89b79ffe204a01594509360
zirui.wang@yandex.com <zirui.wang@yandex.com>
2023-04-24 00:22:41 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+#11
+# n=int(input())
+# coins=input()
+# heads=coins.count("H")
+# tails=coins.count("T")
+# print(abs(n//2-max(heads,tails)))
+
+#2
+# s=int(input("Please enter the sum of theese two numbers"))
+# p=int(input("please enter the product of the two numbers"))
+# for x in range(1,1001):
+#     if p%x==0 and p//x+x==s:
+#         y=p//x
+#         print(x,y)
+#         break
+# else:
+#     print("no solution")
+
+#3
+n=int(input("Please enter a number n "))
+power = 1
+while power <= n:
+    print(power)
+    power *= 2
